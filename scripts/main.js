621017ae3b83bf20f31bebcadc881846c8ca2124
Kenneth Mancini <kennethjmancini@gmail.com>
2016-12-24 13:00:58 -0500

    surf spots array and display spots function

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,26 @@ console.log('js loaded');
 $(function () {
     console.log('jQuery loaded');
 });
+
+var surfSpots = [
+  'Point Judith \'The Lighthouse\'',
+  'Matunuck',
+  'Narragansett Beach',
+  'The K',
+  'Bonnet Point',
+  'Ruggles',
+  'First Beach',
+  'Second Beach',
+  'Monahan\'s Dock'
+];
+
+$(function displaySpots() {
+  var spotsUl = $('ul').addClass('surfSpotsUl');
+
+  for (var i = 0; i < surfSpots.length; i++) {
+    var spotsLi = $('li');
+    spotsLi.text(surfSpots[i].split(' '));
+    spotsUl.append(spotsLi);
+  }
+  $('#surf-spots').append(spotsUl);
+});
