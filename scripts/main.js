988545a13136d23f9fc71ecee8ec08a50621e7c9
Kenneth Mancini <kennethjmancini@gmail.com>
2017-01-11 20:00:41 -0500

    test for weather underground api

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,63 @@
-console.log('js loaded');
+console.log('js connected');
 
-$(function () {
-    console.log('jQuery loaded');
-});
+$(document).ready(function() {
 
-var surfSpots = [
-  'Point Judith \'The Lighthouse\'',
-  'Matunuck',
-  'Narragansett Beach',
-  'The K',
-  'Bonnet Point',
-  'Ruggles',
-  'First Beach',
-  'Second Beach',
-  'Monahan\'s Dock'
-];
+  var WEATHER_UNDERGROUND_API = 'http://api.wunderground.com/api/b9a4b3dc52783dbb/conditions/q/';
 
-$(function displaySpots() {
-  var spotsUl = $('ul').addClass('surfSpotsUl');
+  var testCity = WEATHER_UNDERGROUND_API + 'CA/San_Francisco.json';
 
-  for (var i = 0; i < surfSpots.length; i++) {
-    var spotsLi = $('li');
-    spotsLi.text(surfSpots[i].split(' '));
-    spotsUl.append(spotsLi);
+  var forecastResults = $('#forecast-results');
+
+  function testCallWeatherAPI() {
+    $.get(testCity, function(whatever){
+      console.log(whatever);
+    })
   }
-  $('#surf-spots').append(spotsUl);
+
+  testCallWeatherAPI();
+
+  function callWeatherAPI(city) {
+    if (city.length == 0) {
+      forecastResults.html('Please enter a city name into the search bar');
+    } else {
+      $.get(WEATHER_UNDERGROUND_API + city, function(forecastResults) {
+
+      });
+    }
+  }
+
+  $('#search-button').on('click', function(e) {
+    e.preventDefault();
+
+    callWeatherAPI($('#search-bar').val());
+  });
+
+//
+  var surfSpots = [
+    'Point Judith \'The Lighthouse\'',
+    'Matunuck',
+    'Narragansett Beach',
+    'The K',
+    'Bonnet Point',
+    'Ruggles',
+    'First Beach',
+    'Second Beach',
+    'Monahan\'s Dock'
+  ];
+
+  $('#search-button').on('click', function() {
+    console.log('search button clicked!');
+
+    for (var i = 0; i < surfSpots.length; i++) {
+      var spotsLi = $('li').text(surfSpots[i]);
+      $('#surf-spots-ul').append(spotsLi);
+    }
+  });
+
+  $('#surf-spots').each(surfSpots, function(e) {
+    var spotsLi = $('li');
+    $(spotsLi).text(surfSpots[e]);
+    $(spotsLi).appendTo('#surf-spots-ul');
+  })
+
 });
