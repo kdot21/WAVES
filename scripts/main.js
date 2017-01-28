fc6307b2799640a96227d8d632f1b0e8a92bf9bc
Kenneth Mancini <kennethjmancini@gmail.com>
2017-01-28 11:47:50 -0500

    added google maps api

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,85 @@
-console.log('js connected');
+console.log('js!');
+
+  // Google Maps API
+  var map;
+
+  function initMap() {
+
+    var styles = [
+      {
+        stylers: [
+          { hue: '#00c5cd' },
+          { saturation: -20 }
+        ]
+      }, {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [
+            { lightness: 100 },
+            { visibility: 'simplified' }
+          ]
+      }, {
+          featureType: 'road',
+          elementType: 'labels',
+          stylers: [
+            { visibility: 'off' }
+          ]
+      }
+    ];
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 41.361624, lng: -71.481146},
+      zoom: 15,
+      map: map,
+      zoomControl: true,
+      mapTypeControl: true,
+      streetViewControl: true,
+      rotateControl: true,
+      fullscreenControl: true,
+      styles: styles
+    });
+
+    var marker = new google.maps.Marker({
+      position: {lat: 41.361624, lng: -71.481146},
+      map: map
+    });
+
+  }
 
 $(document).ready(function() {
+  console.log('jQuery!')
+  // // Weather Underground API for search capabilites
+  // var WEATHER_UNDERGROUND_API = 'http://api.wunderground.com/api/b9a4b3dc52783dbb/conditions/q/';
 
-  var WEATHER_UNDERGROUND_API = 'http://api.wunderground.com/api/b9a4b3dc52783dbb/conditions/q/';
+  // var testCity = WEATHER_UNDERGROUND_API + 'CA/San_Francisco.json';
 
-  var testCity = WEATHER_UNDERGROUND_API + 'CA/San_Francisco.json';
+  // var forecastResults = $('#forecast-results');
 
-  var forecastResults = $('#forecast-results');
+  // function testCallWeatherAPI() {
+  //   $.get(testCity, function(whatever){
+  //     console.log(whatever);
+  //   })
+  // }
 
-  function testCallWeatherAPI() {
-    $.get(testCity, function(whatever){
-      console.log(whatever);
-    })
-  }
+  // testCallWeatherAPI();
 
-  testCallWeatherAPI();
+  // function callWeatherAPI(city) {
+  //   if (city.length == 0) {
+  //     forecastResults.html('Please enter a city name into the search bar');
+  //   } else {
+  //     $.get(WEATHER_UNDERGROUND_API + city, function(forecastResults) {
 
-  function callWeatherAPI(city) {
-    if (city.length == 0) {
-      forecastResults.html('Please enter a city name into the search bar');
-    } else {
-      $.get(WEATHER_UNDERGROUND_API + city, function(forecastResults) {
+  //     });
+  //   }
+  // }
 
-      });
-    }
-  }
+  // $('#search-button').on('click', function(e) {
+  //   e.preventDefault();
 
-  $('#search-button').on('click', function(e) {
-    e.preventDefault();
+  //   callWeatherAPI($('#search-bar').val());
+  // });
 
-    callWeatherAPI($('#search-bar').val());
-  });
-
-//
+  // Appending array of surf spots
   var surfSpots = [
     'Point Judith \'The Lighthouse\'',
     'Matunuck',
@@ -45,19 +92,14 @@ $(document).ready(function() {
     'Monahan\'s Dock'
   ];
 
-  $('#search-button').on('click', function() {
-    console.log('search button clicked!');
+  $('#search-button').on('mouseover', function() {
 
-    for (var i = 0; i < surfSpots.length; i++) {
-      var spotsLi = $('li').text(surfSpots[i]);
-      $('#surf-spots-ul').append(spotsLi);
-    }
+    $.each(surfSpots, function(i,e) {
+      var spotsLi = "<li>";
+      spotsLi += e + "</li>";
+      $(spotsLi).appendTo('#surf-spots-ul');
+
+    });
   });
-
-  $('#surf-spots').each(surfSpots, function(e) {
-    var spotsLi = $('li');
-    $(spotsLi).text(surfSpots[e]);
-    $(spotsLi).appendTo('#surf-spots-ul');
-  })
 
 });
